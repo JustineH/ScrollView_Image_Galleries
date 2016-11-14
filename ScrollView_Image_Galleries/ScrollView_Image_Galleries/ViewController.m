227868091130ab731673354b1c0552a0577ffc4f
Justine <justine.m.h@gmail.com>
2016-11-14 14:09:40 -0800

    Initial commit

---
+++ ScrollView_Image_Galleries/ScrollView_Image_Galleries/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ScrollView_Image_Galleries
+//
+//  Created by Justine Herman on 11/14/16.
+//  Copyright © 2016 Justine Herman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ScrollView_Image_Galleries/ScrollView_Image_Galleries/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScrollView_Image_Galleries
+//
+//  Created by Justine Herman on 11/14/16.
+//  Copyright © 2016 Justine Herman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
