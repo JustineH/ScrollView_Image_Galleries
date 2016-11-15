d38533c267a1a05d56834264b695cc94f14ddb2c
Justine <justine.m.h@gmail.com>
2016-11-14 21:29:34 -0800

    Second view set up

---
+++ ScrollView_Image_Galleries/ScrollView_Image_Galleries/DetailsViewController.m
@@ -0,0 +1,52 @@
+//
+//  DetailsViewController.m
+//  ScrollView_Image_Galleries
+//
+//  Created by Justine Herman on 11/14/16.
+//  Copyright © 2016 Justine Herman. All rights reserved.
+//
+
+#import "DetailsViewController.h"
+#import "ViewController.h"
+
+
+@interface DetailsViewController () <UIScrollViewDelegate>
+
+@property (nonatomic, strong) UIImageView *lighthouseImageView;
+@property (weak, nonatomic) IBOutlet UIScrollView *imageScrollView;
+
+
+@end
+
+@implementation DetailsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.imageScrollView.delegate = self;
+    
+    
+    self.lighthouseImageView = [[UIImageView alloc] initWithImage:self.detailedImage];
+    self.lighthouseImageView.frame = self.view.frame;
+    self.lighthouseImageView.contentMode = UIViewContentModeScaleAspectFit;
+    
+    [self.imageScrollView addSubview:self.lighthouseImageView];
+    
+    self.imageScrollView.contentSize = self.lighthouseImageView.bounds.size;
+}
+
+- (void)setupPanningAndZoomingImage
+{
+    self.imageScrollView.minimumZoomScale = 0.5;
+    self.imageScrollView.maximumZoomScale = 3.0;
+    
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.lighthouseImageView;
+}
+
+
+@end
+
--- ScrollView_Image_Galleries/ScrollView_Image_Galleries/ViewController.m
+++ ScrollView_Image_Galleries/ScrollView_Image_Galleries/ViewController.m
@@ -7,23 +7,71 @@
 //
 
 #import "ViewController.h"
+#import "DetailsViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *imageScrollView;
+@property (strong, nonatomic) UIImageView *lighthouseImageView;
+@property (strong, nonatomic, readonly) NSArray<UIImage *> *lighthouseImages;
+
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self scrollViewImages];
+
 }
 
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (void)scrollViewImages
+{
+    
+    CGFloat imageXPosition = 0;
+    
+    CGFloat scrollViewWidth = CGRectGetWidth(self.view.frame);
+    CGFloat scrollViewHeight = CGRectGetHeight(self.imageScrollView.frame);
+    
+    for (UIImage *lighthouseImage in self.lighthouseImages)
+    {
+        UIImageView *lighthouseImageView = [[UIImageView alloc] initWithImage:lighthouseImage];
+        
+        lighthouseImageView.frame = CGRectMake(imageXPosition, 0, scrollViewWidth, scrollViewHeight);
+        lighthouseImageView.contentMode = UIViewContentModeScaleAspectFit;
+        
+        [self.imageScrollView addSubview:lighthouseImageView];
+        
+        imageXPosition += scrollViewWidth;
+        
+    }
+    
+    self.imageScrollView.contentSize = CGSizeMake(scrollViewWidth*self.lighthouseImages.count, scrollViewHeight);
+    
+    self.imageScrollView.pagingEnabled = YES;
+    
+}
+
+- (NSArray<UIImage *> *)lighthouseImages
+{
+    return @
+    [
+        [UIImage imageNamed:@"Lighthouse"],
+        [UIImage imageNamed:@"Lighthouse-night"],
+        [UIImage imageNamed:@"Lighthouse-in-Field"],
+     
+     ];
+}
+
 
 @end
+
